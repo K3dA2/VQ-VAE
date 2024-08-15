30c04e8228a42e21d9a10864c14a88a8a13428ec
K3dA2 <ayanfeoluwaadekanye1@gmail.com>
2024-08-15 22:35:19 +0100

    Update train.py

--- train.py
+++ train.py
@@ -11,8 +11,6 @@ from model import VQVAE
 from utils import get_data_loader, count_parameters, save_img_tensors_as_grid
 import uuid
 
-
-
 def training_loop(n_epochs, optimizer, model, loss_fn, device, data_loader, valid_loader,
                   max_grad_norm=1.0, epoch_start=0, save_img=True, show_img=False,
                 ema_alpha=0.99,usage_threshold=1.0):
@@ -67,9 +65,27 @@ def training_loop(n_epochs, optimizer, model, loss_fn, device, data_loader, vali
 
         with open("waifu-vqvae_epoch-loss.txt", "a") as file:
             file.write(f"{avg_loss_train}\n")
+        
+        
 
         print('{} Epoch {}, Training loss {:.4f}, MSE loss {:.4f}, VQ loss {:.4f}'.format(
             datetime.datetime.now(), epoch, avg_loss_train, avg_mse_loss_train, avg_vq_loss_train))
+
+        if epoch % 5 == 0:
+            # Validation phase
+            model.eval()
+            loss_val = 0.0
+            with torch.no_grad():
+                for imgs, _ in valid_loader:
+                    imgs = imgs.to(device)
+                    outputs, vq_loss = model(imgs)
+                    mse_loss = loss_fn(outputs, imgs)
+                    loss = mse_loss + vq_loss
+                    loss_val += loss.item()
+
+            avg_loss_val = loss_val / len(valid_loader)
+            print(f'Val loss: {avg_loss_val}')
+
 
         if epoch % 5 == 0:
             if save_img:
@@ -81,14 +97,14 @@ def training_loop(n_epochs, optimizer, model, loss_fn, device, data_loader, vali
                     val_img, _ = model(valid_tensors.to(device))
                     save_img_tensors_as_grid(val_img, 4, "recon1")
 
-            model_path = os.path.join('./', 'waifu-vqvae_epoch.pth')
+            model_path = os.path.join('weights/', 'waifu-vqvae_epoch.pth')
             torch.save({
                 'epoch': epoch,
                 'model_state_dict': model.state_dict(),
                 'optimizer_state_dict': optimizer.state_dict(),
             }, model_path)
             # Reset underused embeddings
-        '''
+        
         # Reset underused embeddings conditionally
         if epoch > 1000 and previous_loss is not None and avg_loss_train > previous_loss * 1.25:
             print("reseting")
@@ -98,14 +114,13 @@ def training_loop(n_epochs, optimizer, model, loss_fn, device, data_loader, vali
                     break
 
         previous_loss = avg_loss_train
-        '''
 
 
 
 if __name__ == "__main__":
     path = '/Users/ayanfe/Documents/Datasets/Waifus/Train'
     val_path = '/Users/ayanfe/Documents/Datasets/Waifus/Val'
-    model_path = '/Users/ayanfe/Documents/Code/VQ-VAE/weights/waifu-vqvae_epoch.pth'
+    model_path = '/Users/ayanfe/Documents/Code/simple vq-vae/weights/waifu-vqvae_epoch.pth'
     epoch = 0
 
     device = "cpu"
@@ -115,24 +130,23 @@ if __name__ == "__main__":
         device = "mps"
     print(f"using device: {device}")
 
-    model = VQVAE(latent_dim=1, num_embeddings=512, beta=0.25, use_ema=False)  # Assuming Unet is correctly imported and defined
+    model = VQVAE(latent_dim = 12, num_embeddings=512, beta=0.25, use_ema=False, e_width=64,d_width=64)  # Assuming Unet is correctly imported and defined
     model.to(device)
-    optimizer = optim.AdamW(model.parameters(), lr=2e-4)
+    optimizer = optim.AdamW(model.parameters(), lr=5e-4)
     loss_fn = nn.MSELoss().to(device)
 
     print(count_parameters(model))
-    data_loader = get_data_loader(path, batch_size=64, num_samples=30_000)
-    val_loader = get_data_loader(val_path, batch_size=64, num_samples=10_000)
+    data_loader = get_data_loader(path, batch_size = 64, num_samples=80_000)
+    val_loader = get_data_loader(val_path, batch_size = 64, num_samples=10_000)
 
-    '''
+    
     # Optionally load model weights if needed
     checkpoint = torch.load(model_path)
     model.load_state_dict(checkpoint['model_state_dict'])
     optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
     epoch = checkpoint['epoch']
-    '''
     
-    #print(model.inference(1, 16, 16).shape)
+    
     with torch.no_grad():
         for valid_tensors, _ in val_loader:
             break
@@ -141,8 +155,9 @@ if __name__ == "__main__":
         val_img, _ = model(valid_tensors.to(device))
         save_img_tensors_as_grid(val_img, 4, "recon1")
 
+    
     training_loop(
-        n_epochs=200,
+        n_epochs=300,
         optimizer=optimizer,
         model=model,
         loss_fn=loss_fn,
@@ -151,3 +166,4 @@ if __name__ == "__main__":
         valid_loader=val_loader,
         epoch_start=epoch + 1,
     )
+    
